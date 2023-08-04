98d246c75ad304e061582c763b73d9cb7f7e8bd0
Sumandeep Banerjee <sumandeep.banerjee@gmail.com>
2023-08-05 00:21:58 +0530

    add: c++ srv cli classes

--- src/cpp_srvcli/src/add_two_ints_server.cpp
+++ src/cpp_srvcli/src/add_two_ints_server.cpp
@@ -1,28 +1,36 @@
-#include "rclcpp/rclcpp.hpp"
-#include "example_interfaces/srv/add_two_ints.hpp"
-
+#include <functional>
 #include <memory>
 
-void add(const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request,
-          std::shared_ptr<example_interfaces::srv::AddTwoInts::Response>      response)
-{
-  response->sum = request->a + request->b;
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request\na: %ld" " b: %ld",
-                request->a, request->b);
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%ld]", (long int)response->sum);
-}
+#include "example_interfaces/srv/add_two_ints.hpp"
+#include "rclcpp/rclcpp.hpp"
 
-int main(int argc, char **argv)
-{
-  rclcpp::init(argc, argv);
+using namespace std::placeholders;
+class AdditionServer : public rclcpp::Node {
+private:
+    rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service_;
 
-  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("add_two_ints_server");
+    void add_callback(const std::shared_ptr<rmw_request_id_t> request_header,
+                      const std::shared_ptr<example_interfaces::srv::AddTwoInts::Request> request,
+                      std::shared_ptr<example_interfaces::srv::AddTwoInts::Response> response) {
+        (void)request_header;
+        response->sum = request->a + request->b;
+        RCLCPP_INFO(this->get_logger(), "AdditionServer: Incoming request\na: %ld b: %ld",
+                    (long)request->a, (long)request->b);
+        RCLCPP_INFO(this->get_logger(), "AdditionServer: sending back response: [%ld]",
+                    (long)response->sum);
+    }
 
-  rclcpp::Service<example_interfaces::srv::AddTwoInts>::SharedPtr service =
-    node->create_service<example_interfaces::srv::AddTwoInts>("add_two_ints", &add);
+public:
+    AdditionServer() : Node("addition_server") {
+        service_ = this->create_service<example_interfaces::srv::AddTwoInts>("add_two_ints",
+                                                                             std::bind(&AdditionServer::add_callback, this, _1, _2, _3));
+        RCLCPP_INFO(this->get_logger(), "AdditionServer: Ready to add two ints.");
+    }
+};
 
-  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready to add two ints.");
-
-  rclcpp::spin(node);
-  rclcpp::shutdown();
+int main(int argc, char **argv) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<AdditionServer>());
+    rclcpp::shutdown();
+    return 0;
 }

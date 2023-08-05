515aa6156ca9c770e055d4072291bd353169b473
sumandeepb <sumandeep.banerjee@gmail.com>
2023-08-05 12:32:48 +0000

    update: custom msg srv tutorial

--- src/cpp_pubsub/src/subscriber_member_function.cpp
+++ src/cpp_pubsub/src/subscriber_member_function.cpp
@@ -16,32 +16,34 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+// #include "std_msgs/msg/string.hpp"
+#include "tutorial_interfaces/msg/num.hpp" // CHANGE
 
 using std::placeholders::_1;
 
-class MinimalSubscriber : public rclcpp::Node
-{
+class MinimalSubscriber : public rclcpp::Node {
 public:
-  MinimalSubscriber()
-  : Node("minimal_subscriber")
-  {
-    subscription_ = this->create_subscription<std_msgs::msg::String>(
-      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
-  }
+    MinimalSubscriber()
+        : Node("minimal_subscriber") {
+        // subscription_ = this->create_subscription<std_msgs::msg::String>(
+        subscription_ = this->create_subscription<tutorial_interfaces::msg::Num>( // CHANGE
+            "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+    }
 
 private:
-  void topic_callback(const std_msgs::msg::String & msg) const
-  {
-    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
-  }
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+    // void topic_callback(const std_msgs::msg::String & msg) const
+    void topic_callback(const tutorial_interfaces::msg::Num &msg) const // CHANGE
+    {
+        // RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
+        RCLCPP_INFO_STREAM(this->get_logger(), "I heard: '" << msg.num << "'"); // CHANGE
+    }
+    // rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+    rclcpp::Subscription<tutorial_interfaces::msg::Num>::SharedPtr subscription_; // CHANGE
 };
 
-int main(int argc, char * argv[])
-{
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalSubscriber>());
-  rclcpp::shutdown();
-  return 0;
+int main(int argc, char *argv[]) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<MinimalSubscriber>());
+    rclcpp::shutdown();
+    return 0;
 }
